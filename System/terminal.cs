6d0515e98e9d2d13609d83f041b3b3f135f2264b
Cangjier <moodlee@qq.com>
2025-11-17 22:06:36 +0800

    add terminal

---
+++ System/terminal.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Concurrent;
+using System.Text;
+using TidyHPC.LiteJson;
+using TidyHPC.Terminal;
+
+namespace Cangjie.TypeSharp.System;
+/// <summary>
+/// 终端管理
+/// </summary>
+public class terminal
+{
+    public class terminalOptions(Json target)
+    {
+        private Json Target = target;
+
+        public static implicit operator terminalOptions(Json target) => new(target);
+
+        public string? shell
+        {
+            get => Target.Read(nameof(shell), null!);
+            set => Target[nameof(shell)] = value;
+        }
+
+        public string? workingDirectory
+        {
+            get => Target.Read(nameof(workingDirectory), null!);
+            set => Target[nameof(workingDirectory)] = value;
+        }
+
+        public Json environmentVariables
+        {
+            get => Target.Get(nameof(environmentVariables), Json.Null);
+            set => Target[nameof(environmentVariables)] = value;
+        }
+
+        public int columns
+        {
+            get => Target.Read(nameof(columns), 80);
+            set => Target[nameof(columns)] = value;
+        }
+
+        public int rows
+        {
+            get => Target.Read(nameof(rows), 24);
+            set => Target[nameof(rows)] = value;
+        }
+    }
+
+    public class terminalWrapper
+    {
+        public terminalWrapper(ITerminal target)
+        {
+            Target = target;
+            target.OutputReceived += (bytes, length) =>
+            {
+                OnData?.Invoke(bytes, length);
+            };
+        }
+
+        private static UTF8Encoding utf8 = new();
+
+        public ITerminal Target { get; }
+
+        private Action<byte[], int>? OnData;
+
+        public void onOutput(Action<byte[], int> callback)
+        {
+            OnData = callback;
+        }
+
+        public async Task writeAsync(Json data)
+        {
+            if (data.IsString)
+            {
+                var bytes = utf8.GetBytes(data.AsString);
+                await Target.WriteInputAsync(bytes, bytes.Length);
+            }
+            else if (data.Is<byte[]>())
+            {
+                var bytes = data.As<byte[]>();
+                await Target.WriteInputAsync(bytes, bytes.Length);
+            }
+            else
+            {
+                throw new Exception($"Unsupported data type {data.Node?.GetType().Name} for terminal writeAsync");
+            }
+        }
+
+        public async Task start(terminalOptions options)
+        {
+            var translatedOptions = new TerminalOptions();
+            if (options.shell != null)
+            {
+                translatedOptions.Shell = options.shell;
+            }
+            if (options.workingDirectory != null)
+            {
+                translatedOptions.WorkingDirectory = options.workingDirectory;
+            }
+            if (options.environmentVariables.IsObject)
+            {
+                foreach (var pair in options.environmentVariables.GetObjectEnumerable())
+                {
+                    translatedOptions.EnvironmentVariables[pair.Key] = pair.Value.AsString;
+                }
+            }
+            translatedOptions.Columns = options.columns;
+            translatedOptions.Rows = options.rows;
+            await Target.StartAsync(translatedOptions);
+        }
+
+
+    }
+
+    private static ConcurrentDictionary<Guid, terminalWrapper> Terminals { get; } = [];
+
+    public static async Task<terminalWrapper> createAsync(terminalOptions options)
+    {
+        var terminal = new terminalWrapper(ITerminal.CreateTerminal());
+        await terminal.start(options);
+        Terminals[terminal.Target.ID] = terminal;
+        return terminal;
+    }
+
+    public static void close(Guid id)
+    {
+        if (Terminals.TryRemove(id, out var terminal))
+        {
+            terminal.Target.Dispose();
+        }
+    }
+
+    public static terminalWrapper get(Guid id)
+    {
+        if (Terminals.TryGetValue(id, out var terminal))
+        {
+            return terminal;
+        }
+        throw new Exception($"Terminal with ID {id} not found");
+    }
+}
