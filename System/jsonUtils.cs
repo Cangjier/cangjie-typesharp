c392495cbe4e87ac588cdfbf9a8345d2472371f3
Cangjier <moodlee@qq.com>
2026-03-08 13:10:03 +0800

    Add replaceKeyValue method to jsonUtils class for enhanced JSON manipulation
    Introduced a new replaceKeyValue method in the jsonUtils class to allow for key-value replacements in JSON objects. This addition enhances the functionality of the class, providing more flexibility in JSON data manipulation while maintaining existing methods for value replacement.

--- System/jsonUtils.cs
+++ System/jsonUtils.cs
@@ -3,8 +3,16 @@
 namespace Cangjie.TypeSharp.System;
 public class jsonUtils
 {
-    public static void replaceValue(Json value,Func<Json,Json> onValue)
+    public static void replaceValue(Json value, Func<Json, Json> onValue)
     {
         value.EachAll(onValue);
+    }
+
+    public static void replaceKeyValue(Json value, Func<Json, Json, Json> onKeyValue)
+    {
+        value.EachAll((key, value) =>
+        {
+            return onKeyValue(key.ToJson(), value);
+        });
     }
 }
